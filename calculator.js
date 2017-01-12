58cd3b8051e9191fa7378b8964ab4e8ab17efe48
Pierce Dyal <mac8@129mac18.valleychristian.net>
2017-01-12 13:54:25 -0800

    1.0

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 
-//Change Display
+// Change Display
 function d(val) {
 	document.getElementById("d").value = val;
 }
@@ -10,12 +10,12 @@ function v(val) {
 	document.getElementById("d").value += val;
 }
 
-//Evaluate the equation
+// Evaluate the equation
 function e() {
 	try {
-		  d(eval(document.getElementById("d").value));
+		d(eval(document.getElementById("d").value));
 	}
 	catch(err) {
-			d("Error");
+		d("Error");
 	}
 }
